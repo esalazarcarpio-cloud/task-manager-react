a6ab06438de07d5616285012e4c3a3f6470a9c00
esalazarcarpio-cloud <esalazarcarpio@gmail.com>
2026-07-15 00:11:55 -0400

    test:laboratorio2

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,22 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/e2e/**',
+    ], 
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        lines: 60,
+        functions: 60,
+        branches: 50,
+        statements: 60,
+      },
+    }
+
   },
 
 })
